59a3548af0ca1b3b328b30beb024866e475c4371
dmitrykachala <kosas@mail.ru>
2021-11-01 14:32:40 +0300

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,28 +4,72 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Parse;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.Post;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
-public class SqlRuParse {
-    public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+import java.util.ArrayList;
+import java.util.List;
 
-        Elements pages = doc.select("td:contains(страницы)");
+public class SqlRuParse implements Parse {
 
-        for (int i = 1; i < 6; i++) {
+    private final DateTimeParser dateTimeParser;
 
-            Elements row = doc.select(".postslisttopic");
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
 
-            for (Element td : row) {
-                Element href = td.child(0);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                System.out.println(td.parent().child(5).text());
-            }
+    public static void main(String[] args) {
 
-            doc = Jsoup.connect("https://www.sql.ru/forum/job-offers"
-                    + "/" + (i + 1)).get();
-            System.out.println(System.lineSeparator());
+        SqlRuParse sqP = new SqlRuParse(new SqlRuDateTimeParser());
+        sqP.list("https://www.sql.ru/forum/job-offers").forEach(System.out::println);
 
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        List<Post> listPost = new ArrayList<>();
+        try {
+        Document doc = Jsoup.connect(link).get();
+        Elements row = doc.select(".postslisttopic");
+        for (Element td : row) {
+            Element href = td.child(0);
+            listPost.add(detail(href.attr("href")));
         }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listPost;
+    }
+
+    @Override
+    public Post detail(String link) {
+        Post post = new Post();
+        try {
+            Document doc = Jsoup.connect(link).get();
+
+            post.setLink(link);
+
+            Elements titleAll = doc.select("td.messageHeader");
+            Element title = titleAll.first();
+
+            post.setTitle(title.text());
+
+            Elements description = doc.select(".msgBody:not(.style)");
+
+            post.setDescription(description.next().text());
+
+            Elements created = doc.select("td.msgFooter");
+            String[] rawDate = created.first().text().split(" \\[");
+            String dateStr = rawDate[0];
+
+            post.setCreated(dateTimeParser.parse(dateStr));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return post;
     }
 }
