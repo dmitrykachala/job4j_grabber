4c3f3c5679bb04ed3a2b2ba24a7e519b95bcd39b
dmitryu.kachala <kosas@mail.ru>
2021-11-13 01:44:10 +0300

    1. Реализация кеша на SoftReference [#1592 #226661]

--- src/main/java/ru/job4j/cache/AbstractCache.java
+++ src/main/java/ru/job4j/cache/AbstractCache.java
@@ -9,12 +9,15 @@ public abstract class AbstractCache<K, V> {
     private Map<K, SoftReference<V>> cache = new HashMap<>();
 
     public void put(K key, V value) {
+        if (load(key).equals("") || load(key) == null) {
+            System.out.println("Achtung!");
+        }
         SoftReference<V> valSoftRef = new SoftReference<>(value);
         cache.put(key, valSoftRef);
     }
 
     public V get(K key) {
-        if (cache.get(key) == null) {
+        if (cache.get(key).get() == null) {
             put(key, load(key));
         }
         return cache.get(key).get();
