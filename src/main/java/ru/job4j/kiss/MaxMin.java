d96464162f814b642f115096d40a800f530c62dc
dmitryu.kachala <kosas@mail.ru>
2021-11-12 23:48:42 +0300

    1. Принципы Kiss, Dry и Yagni [#238813 #227905]

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -2,23 +2,29 @@ package ru.job4j.kiss;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 
 public class MaxMin {
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        T max = null;
-        if (value.size() > 0) {
-            max = value.get(0);
-            for (T t : value) {
-                if (comparator.compare(max, t) < 0) {
-                    max = t;
-                }
-            }
-        }
-        return max;
+        return compare(value, (p1, p2) -> comparator.compare(p1, p2) >= 0);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        return max(value, comparator.reversed());
+        return compare(value, (p1, p2) -> comparator.compare(p1, p2) < 0);
+    }
+
+    public <T> T compare(List<T> value, BiPredicate<T, T> predicate) {
+        T rsl = null;
+        if (value.size() > 0) {
+            rsl = value.get(0);
+            for (T t : value) {
+                if (predicate.test(t, rsl)) {
+                    rsl = t;
+                }
+            }
+        }
+        return rsl;
     }
 
 }
--- src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -2,6 +2,7 @@ package ru.job4j.kiss;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -12,16 +13,24 @@ public class MaxMinTest {
     @Test
     public void max() {
         MaxMin mm = new MaxMin();
-        int rsl = mm.max(List.of(34, 23, 0, 1), Comparator.comparingInt(x -> x));
-        int expected = 34;
+        int rsl = mm.max(List.of(-1, 34, 23, 0, 1, 56), Comparator.comparingInt(x -> x));
+        int expected = 56;
         assertEquals(expected, rsl);
     }
 
     @Test
     public void min() {
         MaxMin mm = new MaxMin();
-        int rsl = mm.min(List.of(34, 23, 0, 1), Comparator.comparingInt(x -> x));
-        int expected = 0;
+        int rsl = mm.min(List.of(-17, 34, 23, 0, 1, -2), Comparator.comparingInt(x -> x));
+        int expected = -17;
+        assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void compare() {
+        MaxMin mm = new MaxMin();
+        int rsl = mm.compare(List.of(34, 23, 0, 1), (x, y) -> x - y == 11);
+        int expected = 34;
         assertEquals(expected, rsl);
     }
 }
